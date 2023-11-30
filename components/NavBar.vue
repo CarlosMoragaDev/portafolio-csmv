4043145d819e5e8b76bbf126cd51f35beebc4fb7
Carlos Moraga <cmoraga@tico.cl>
2023-11-30 09:18:11 -0300

    update navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -25,48 +25,21 @@ const scrollToSection = (target: any) => {
 
 <template>
   <header>
-    <nav style="background: #688ba1" class="fixed top-0 left-0 w-full bg-white shadow-md z-50">
+    <nav style="background: #688ba1" class="lg:fixed top-0 left-0 w-full shadow-md">
       <div class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
-        <div class="flex lg:flex-1" >
-        <a href="#" class="-m-1.5 p-1.5">
-          <!-- <span class="sr-only">Your Company</span>
+        <div class="flex lg:flex-1">
+          <a href="#" class="-m-1.5 p-1.5">
+            <!-- <span class="sr-only">Your Company</span>
           <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> -->
-        </a>
-      </div>
-      <div class="flex lg:hidden">
-        <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
-          <span class="sr-only">Abrir menú principal</span>
-          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
-        </button>
-      </div>
-      <div class="hidden lg:flex lg:gap-x-12">
-        <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm font-semibold leading-6 text-ming-900 hover:text-emerald-500 scroll-smooth"
-        @click.prevent="scrollToSection(item.href)">{{ item.name }}</a>
-      </div>
+          </a>
+        </div>
+        <div class="lg:flex lg:gap-x-12 sm:flex sm:gap-x-4">
+          <a v-for="item in navigation" :key="item.name" :href="item.href"
+            class="text-sm font-semibold leading-6 text-ming-900 hover:text-emerald-500 scroll-smooth mx-2 "
+            @click.prevent="scrollToSection(item.href)">{{ item.name }}</a>
+        </div>
       </div>
     </nav>
-    <!-- <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
-      <div class="fixed inset-0 z-10" />
-      <DialogPanel class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-        <div class="flex items-center justify-between">
-          <a href="#" class="-m-1.5 p-1.5">
-            <span class="sr-only">Your Company</span>
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
-          </a>
-          <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
-            <span class="sr-only">Close menu</span>
-            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
-        <div class="mt-6 flow-root">
-          <div class="-my-6 divide-y divide-gray-500/10">
-            <div class="space-y-2 py-6">
-              <a v-for="item in navigation" :key="item.name" :href="item.href" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-ming-900 hover:text-emerald-500">{{ item.name }}</a>
-            </div>
-          </div>
-        </div>
-      </DialogPanel>
-    </Dialog> -->
   </header>
 </template>
 
